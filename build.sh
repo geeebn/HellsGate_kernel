7be74a39195cb5c7b2c040a7e8258ce6669dfe35
kranone124 <kranone1234@gmail.com>
2017-08-21 17:15:48 +0200

    set my parts

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ RDIR=$(pwd)
 VER=$(cat "$RDIR/VERSION")
 
 # directory containing cross-compile arm64 toolchain
-TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-6.2.1-2016.11-x86_64_aarch64-linux-gnu
+TOOLCHAIN=$home/kranoner/build/toolchain/gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu
 
 CPU_THREADS=$(grep -c "processor" /proc/cpuinfo)
 # amount of cpu threads to use in kernel make process
@@ -36,7 +36,7 @@ cd "$RDIR" || ABORT "Failed to enter $RDIR!"
 
 CONTINUE=false
 export ARCH=arm64
-export CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/home/kranoner/build/toolchain/gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- 
 
 [ -x "${CROSS_COMPILE}gcc" ] ||
 ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
@@ -54,7 +54,7 @@ while [ $# != 0 ]; do
 	shift
 done
 
-[ "$TARGET" ] || TARGET=AX7Kernel
+[ "$TARGET" ] || TARGET=HellsGate-Kernel
 
 DEFCONFIG=${TARGET}_defconfig
 
@@ -113,4 +113,3 @@ fi
 BUILD_KERNEL &&
 INSTALL_MODULES &&
 echo "Finished building $LOCALVERSION!"
-
